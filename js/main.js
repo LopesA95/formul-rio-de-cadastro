081e264f996b2624c7c31987c70844f0f73e92c2
Anderson lopes <andersonlopes017.al@gmail.com>
2024-01-20 01:18:34 -0400

    fix: correções pontuais

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function () {
   var masks = {
-    '#telefone': '(00) 00000-0000',
-    '#cpf': '000.000.000-00',
-    '#cep': '00000-000'
+    "#telefone": "(00) 00000-0000",
+    "#cpf": "000.000.000-00",
+    "#cep": "00000-000",
   };
 
   for (var id in masks) {
     $(id).mask(masks[id]);
   }
 
-  // Adicione um evento de submit ao seu formulário
-  $('#signupForm').on('submit', function(e) {
-    // Verifique se todas as validações foram preenchidas
+  $("#signupForm").on("submit", function (e) {
     var isValid = true;
     for (var id in masks) {
-      if ($(id).val() === '') {
+      if ($(id).val() === "") {
         isValid = false;
         break;
       }
     }
 
-    // Se não estiver válido, evite a submissão do formulário
     if (!isValid) {
       e.preventDefault();
     }
   });
 });
-
